229439716b03787a012b35a92ac715d1771e23cb
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 17:59:21 -0600

    variables and data types

---
+++ src/foundations/variables_and_data_types.py
@@ -0,0 +1,36 @@
+# integer
+x = 10
+
+# float
+y = 23.492
+
+# string (str)
+name = "Joe Doe"
+
+# list
+ids = [4872, 789328, 578823]
+
+# tuple
+point = (12.3, 43.1)
+
+# dictionary (dict)
+user = {
+    "name": "John Doe",
+    "id": 23098402,
+}
+
+# Set (mutable)
+products = {"laptop", "iPod", "headset"}
+
+# Set (immutable)
+final_states = frozenset({"not_started", "in_progress", "completed"})
+
+# boolean (bool)
+is_enabled = True
+
+# None (equivalent to null in some languages)
+result = None
+
+print(
+    x, y, name, ids, point, user, products, final_states, is_enabled, result, sep="\n"
+)
