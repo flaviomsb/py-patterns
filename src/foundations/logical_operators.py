30dbcaca9269cab9074f7d9739c417b7f22040ec
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 19:25:30 -0600

    logical operators

---
+++ src/foundations/logical_operators.py
@@ -0,0 +1,7 @@
+a = 10
+b = 2
+
+print(f"Given that a = {a} and b = {b}")
+print(f"(a > b) and (b == 2) {(a > b) and (b == 2)}")
+print(f"(a > b) or (b >= 2) {(a > b) or (b >= 2)}")
+print(f"not (b < a) {not (b < a)}")
