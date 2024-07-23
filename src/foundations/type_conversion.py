e780bec3d5e9c89999de0595aa98f30dc74b6d55
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 18:29:30 -0600

    type conversion

---
+++ src/foundations/type_conversion.py
@@ -0,0 +1,11 @@
+x = 10
+
+# convert to float
+x = float(x)
+
+print(f"{x} as int then converted into a float {x}")
+
+x = "120"
+y = 5
+
+print(f"x = '{x}' and y = {y} then int(x) / y = {int(x) / y}")
