e6d12fc616bd62dbd59ba40fd69983e8ffa123d2
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 18:15:41 -0600

    variables and data types update

--- src/foundations/variables_and_data_types.py
+++ src/foundations/variables_and_data_types.py
@@ -4,14 +4,29 @@ x = 10
 # float
 y = 23.492
 
+# complex number (wow)
+complex_num = 5 + 3j
+
+# Scientific notation
+epsilon = 10e-3
+
 # string (str)
 name = "Joe Doe"
+
+# string (multiline)
+instructions = """
+    In Python, you can represent scientific notation numbers using the e or E notation.
+    This is commonly used for very large or very small numbers and allows you to express
+    numbers as a product of a coefficient and a power of 10
+"""
 
 # list
 ids = [4872, 789328, 578823]
 
 # tuple
 point = (12.3, 43.1)
+# single element tuple requires trailing comma
+parm = (32,)
 
 # dictionary (dict)
 user = {
@@ -32,5 +47,19 @@ is_enabled = True
 result = None
 
 print(
-    x, y, name, ids, point, user, products, final_states, is_enabled, result, sep="\n"
+    x,
+    y,
+    complex_num,
+    epsilon,
+    name,
+    instructions,
+    ids,
+    point,
+    parm,
+    user,
+    products,
+    final_states,
+    is_enabled,
+    result,
+    sep="\n",
 )
