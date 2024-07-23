78ce68e8d5f2bd1919d3e258d1cc183632c03ef9
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 19:25:53 -0600

    assignment operators

---
+++ src/foundations/assignment_operators.py
@@ -0,0 +1,25 @@
+a = 1e2
+b = 5e-2
+
+print(f"Given that a = {a} and b = {b}")
+
+a += b
+print(f"a += b {a}")
+
+a -= b
+print(f"a -= b {a}")
+
+a *= b
+print(f"a *= b {a}")
+
+a /= b
+print(f"a /= b {a}")
+
+a %= b
+print(f"a %= b {a}")
+
+a **= b
+print(f"a **= b {a}")
+
+a //= b
+print(f"a //= b {a}")
