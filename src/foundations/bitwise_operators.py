748a719ce446560906a49f1676d977764c93d226
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 19:45:21 -0600

    bitwise operators

---
+++ src/foundations/bitwise_operators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 2
+
+print(f"Given that a = {a} and b = {b}")
+print(f"a & b {a & b}")
+print(f"a | a {a | a}")
+print(f"a ^ b {a ^ b}")
+print(f"~a {~a}")
+print(f"a >> b {a >> b}")
+print(f"a << b {a << b}")
