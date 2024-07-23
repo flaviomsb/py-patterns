ca3384333367ec7949f4ea7a98f80affa33c7475
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-22 18:49:14 -0600

    type conversion updates

--- src/foundations/type_conversion.py
+++ src/foundations/type_conversion.py
@@ -9,3 +9,11 @@ x = "120"
 y = 5
 
 print(f"x = '{x}' and y = {y} then int(x) / y = {int(x) / y}")
+
+epsilon = 1e-3
+
+print(f"converting float into string {epsilon} = {str(epsilon)}")
+
+print(f"convert list into tuple {tuple({'a', 'b', 'c'})}")
+
+print(f"convert list into dict {dict(zip({'a', 'b', 'c'}, {1, 2, 3}))}")
