b25679e47dc782fa2258d414965137bab2265e93
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-24 10:27:39 -0600

    membership operators

---
+++ src/foundations/membership_operators.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "mango", "lime"]
+
+print(f"fruits = {fruits}")
+print("\n")
+print(f"apple in fruits = {'apple' in fruits}")
+print(f"raspberry in fruits = {'raspberry' in fruits}")
+print(f"raspberry not in fruits = {'raspberry' not in fruits}")
