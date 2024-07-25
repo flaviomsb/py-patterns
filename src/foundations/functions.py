981c6f9e4975aec3ab237339c0391bf5cc7e57f1
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-07-24 23:33:37 -0600

    nested and lambda functions

--- src/foundations/functions.py
+++ src/foundations/functions.py
@@ -1,24 +1,20 @@
-def sum(n1: int, n2: int) -> int:
+def add(n1: int, n2: int) -> int:
     """adds two numbers"""
     return n1 + n2
 
 
-x = sum(2, 3)
+x = add(2, 3)
 
-print("sum(2, 3) is %d" % x)
-print(sum.__doc__)  # prints function documentation
+print("add(2, 3) is %d" % x)
+print(add.__doc__)  # prints function documentation
 
 
 # global variable
-def addUp(n: int) -> int:
-    global x
-    if x >= 5:
-        x **= 2
-
-    return x + n
+def exp_global(n: int) -> int:
+    return x**n
 
 
-print(f"addUp(2) is {addUp(2)}")
+print(f"exp_global(2) is {exp_global(2)}")
 
 
 # exception handling
@@ -36,3 +32,27 @@ def div(n1, n2) -> float:
 print(div(10, 4))
 print(div(2, 0))
 print(div("sjkd", 4))
+
+
+# nested functions
+def calc_price(raw_price: float, tax=0.1) -> float:
+    def calc_tax() -> float:
+        return raw_price * tax
+
+    return raw_price + calc_tax()
+
+
+print(f"calculate price for 120.0 = {calc_price(120.0)}")
+
+
+# variable-length arguments
+def sumAll(*numbers):
+    return sum(numbers)
+
+
+print(sumAll(1, 2))
+
+# lambda
+numbers = [1, 2, 3, 4, 5]
+cubic = map(lambda x: x**3, numbers)
+print(list(cubic))
