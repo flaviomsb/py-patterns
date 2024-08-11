85161a6dde56fe37883f5a2df82b74f587ac1d1d
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-08-10 23:49:43 -0600

    user class unit test with named parameters

--- src/foundations/oop/test_user.py
+++ src/foundations/oop/test_user.py
@@ -5,19 +5,19 @@ from user import User
 
 class TestUser(unittest.TestCase):
     def test_get_name(self):
-        _user = User("John Doe", 5793238)
+        _user = User(name="John Doe", uuid=5793238)
         self.assertEqual(_user.get_name(), "John Doe")
 
     def test_set_name(self):
-        _user = User("Jane Doe", 198795)
+        _user = User(name="Jane Doe", uuid=198795)
         self.assertEqual(_user.get_name(), "Jane Doe")
-        _user.set_name("Jane Louise Doe")
+        _user.set_name(name="Jane Louise Doe")
         self.assertEqual(_user.get_name(), "Jane Louise Doe")
 
     def test_uuid(self):
-        _user = User("Thomas Doe", 38292)
+        _user = User(name="Thomas Doe", uuid=38292)
         self.assertEqual(_user.uuid, "00038292")
 
     def test_display_name(self):
-        _user = User("Paul Doe", 212392)
+        _user = User(name="Paul Doe", uuid=212392)
         self.assertEqual(_user.display_name(), "Paul Doe (000212392)")
