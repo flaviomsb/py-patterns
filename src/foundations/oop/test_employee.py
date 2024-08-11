77029785ab0bc09ab9468df3089b5f62b9ce4667
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-08-11 13:16:38 -0600

    class and static method tests

--- src/foundations/oop/test_employee.py
+++ src/foundations/oop/test_employee.py
@@ -7,3 +7,9 @@ class TestEmployee(unittest.TestCase):
     def test_display_name(self):
         emp = Employee(name="Paul Doe", uuid=212392, role="Manager")
         self.assertEqual(emp.display_name(), "Paul Doe (000212392) - Manager")
+
+    def test_company_as_class_method(self):
+        self.assertEqual(Employee.company(), "Patterns corp")
+
+    def test_work_schedule(self):
+        self.assertEqual(Employee.work_schedule(), "40 hours / week")
