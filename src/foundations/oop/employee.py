e22c7138e99d0e9736d16cbaa30456ad5c3219aa
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-08-11 13:12:56 -0600

    class method example

--- src/foundations/oop/employee.py
+++ src/foundations/oop/employee.py
@@ -2,9 +2,15 @@ from user import User
 
 
 class Employee(User):
+    company_name = "Patterns corp"
+
     def __init__(self, name: str, uuid: int, role: str):
         super().__init__(name, uuid)
         self.__role = role
 
     def display_name(self) -> str:
         return super().display_name() + " - " + self.__role
+
+    @classmethod
+    def company(cls):
+        return cls.company_name
