3f66ee83c959d6e591314820c9bd8eb4305fe2de
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-08-11 22:19:11 -0600

    scatter plot example

---
+++ src/data-analysis/matplotlib_scatter_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [8, 3, 18, 19, 84, 8, 9, 1]
+y = [23, 1, 32, 30, 90, 83, 77, 5]
+
+plt.scatter(x=x, y=y)
+plt.title("Scatter Plot Example")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
