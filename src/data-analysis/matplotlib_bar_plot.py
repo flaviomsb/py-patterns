462a57dd2cfd7baed2f582958551fa47c117aed5
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-08-19 08:52:11 -0600

    fixed bar plot parameters

--- src/data-analysis/matplotlib_bar_plot.py
+++ src/data-analysis/matplotlib_bar_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 makes = ["GM", "Ford", "Toyota"]
 sales_total = [110, 130, 230]
 
-plt.bar(x=makes, y=sales_total)
+plt.bar(makes, sales_total)
 plt.title("Bar Plot Example")
 plt.xlabel("Makes")
 plt.ylabel("Sales Total")
