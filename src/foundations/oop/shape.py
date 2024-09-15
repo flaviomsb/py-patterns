c4841cc4f19626d2abed1558ce63b7c3250c2189
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-09-14 19:39:11 -0600

    implemented few dunder methods: repr, eq and gt

--- src/foundations/oop/shape.py
+++ src/foundations/oop/shape.py
@@ -5,3 +5,12 @@ class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
+
+    def __eq__(self, other) -> bool:
+        return self.area() == other.area()
+
+    def __gt__(self, other) -> bool:
+        return self.area() > other.area()
+
+    def __repr__(self) -> str:
+        return "{0} w/ area {1}".format(self.__class__.__name__, self.area())
