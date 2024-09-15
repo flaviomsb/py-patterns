c611937f1a12d8b246780c60f69385659059766c
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-09-14 19:40:02 -0600

    expanded circle class test cases

--- src/foundations/oop/test_circle.py
+++ src/foundations/oop/test_circle.py
@@ -7,3 +7,22 @@ class TestCircle(unittest.TestCase):
     def test_area(self):
         cir = Circle(radius=58.9)
         self.assertAlmostEqual(cir.area(), 10898.844649760247)
+
+    def test_radius_type_in_setter(self):
+        with self.assertRaises(TypeError):
+            Circle(radius="hello")
+
+    def test_radius_value_in_setter(self):
+        with self.assertRaises(ValueError):
+            Circle(radius=-10.2)
+
+    def test_repr(self):
+        cir = Circle(radius=54.2)
+        self.assertEqual(repr(cir), "Circle w/ area {0}".format(cir.area()))
+
+    def test_dunder_eq(self):
+        self.assertEqual(Circle(radius=10), Circle(radius=10))
+        self.assertNotEqual(Circle(radius=10.2), Circle(radius=10.1))
+
+    def test_dunder_gt(self):
+        self.assertGreater(Circle(radius=43.23), Circle(radius=42))
