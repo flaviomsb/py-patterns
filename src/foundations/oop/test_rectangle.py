e7b0949e509ba0f412100d27be0dd19196e033a6
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-09-14 19:54:37 -0600

    expanded rectangle class test cases

--- src/foundations/oop/test_rectangle.py
+++ src/foundations/oop/test_rectangle.py
@@ -7,3 +7,14 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         rec = Rectangle(width=100, height=50)
         self.assertEqual(rec.area(), 5000)
+
+    def test_repr(self):
+        rec = Rectangle(width=120, height=80)
+        self.assertEqual(repr(rec), "Rectangle w/ area {0}".format(rec.area()))
+
+    def test_dunder_eq(self):
+        self.assertEqual(Rectangle(130, 50), Rectangle(130, 50))
+        self.assertNotEqual(Rectangle(120, 40), Rectangle(120.001, 40.001))
+
+    def test_dunder_gt(self):
+        self.assertGreater(Rectangle(41.223, 32.433), Rectangle(41.222, 32.432))
