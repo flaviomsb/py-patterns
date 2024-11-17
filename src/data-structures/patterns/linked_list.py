de2cf1b91fc5c6edc28580876c5cc4668adf23b7
Flávio Barboza <flavio.barboza@alchemysystems.com>
2024-11-16 22:52:17 -0600

    linked list reverse method

--- src/data-structures/patterns/linked_list.py
+++ src/data-structures/patterns/linked_list.py
@@ -110,3 +110,16 @@ class LinkedList:
         self.length -= 1
 
         return toRemove
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
