b806294bbcd34449cd9c276fbaaa4afffbb5ccf8
Flavio Barboza <flavio.barboza@gmail.com>
2025-05-21 00:07:30 -0600

    add DoublyLinkedList and DoublyLinkedListNode to init file

--- src/data_structures/__init__.py
+++ src/data_structures/__init__.py
@@ -1,4 +1,6 @@
 from .linked_list import LinkedList
 from .linked_list_node import LinkedListNode
+from .double_linked_list import DoublyLinkedList
+from .doubly_linked_list_node import DoublyLinkedListNode
 
-__all__ = ["LinkedList", "LinkedListNode"]
+__all__ = ["LinkedList", "LinkedListNode", "DoublyLinkedList", "DoublyLinkedListNode"]
