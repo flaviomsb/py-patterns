9513eb6ad188388716206b90b1feafd2e3c59ede
Flavio Barboza <flavio.barboza@gmail.com>
2025-05-21 00:08:40 -0600

    rename prev property as previous

--- src/data_structures/doubly_linked_list_node.py
+++ src/data_structures/doubly_linked_list_node.py
@@ -2,5 +2,5 @@ class DoublyLinkedListNode:
     def __init__(self, value) -> None:
         self.value = value
         self.next: DoublyLinkedListNode | None = None
-        self.prev: DoublyLinkedListNode | None = None
+        self.previous: DoublyLinkedListNode | None = None
 
