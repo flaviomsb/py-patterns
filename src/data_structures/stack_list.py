df4d2ec64967c8ba57cbc86050865ba4fd32587e
Flavio Barboza <flavio.barboza@gmail.com>
2025-06-29 22:45:19 -0600

    stack_list class with push, pop, is_empty and size methods

--- src/data_structures/stack_list.py
+++ src/data_structures/stack_list.py
@@ -1,3 +1,18 @@
 class StackList:
     def __init__(self) -> None:
         self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.size == 0
+
+    @property
+    def size(self):
+        return len(self.items)
