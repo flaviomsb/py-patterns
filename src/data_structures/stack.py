608654cfdc3978e113603e9b8d58c3807f679ba8
Flavio Barboza <flavio.barboza@gmail.com>
2025-06-29 21:40:01 -0600

    stack push method implementation

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -2,5 +2,16 @@ from data_structures.linked_list_node import LinkedListNode
 
 class Stack:
     def __init__(self, value) -> None:
-        self.top = LinkedListNode(value)
-        self.height = 1
+        if value:
+            self.push(value)
+
+    def push(self, value):
+        new_node = LinkedListNode(value)
+
+        if self.top is None:
+            self.top = new_node 
+        else:
+            current_top = self.top
+            new_node.next = current_top
+            self.top = new_node
+        self.height += 1
