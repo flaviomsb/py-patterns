e7750663e985dfb379faf6fc5074635857b1ee04
Flavio Barboza <flavio.barboza@gmail.com>
2025-06-29 22:29:51 -0600

    queue dequeue method

--- src/data_structures/queue.py
+++ src/data_structures/queue.py
@@ -4,16 +4,32 @@ class Queue:
     def __init__(self):
         self.first = None
         self.last = None
-        self.legth = 0
+        self.length = 0
     
     def enqueue(self, value):
         new_node = LinkedListNode(value)
 
-        if self.legth == 0:
+        if self.length == 0:
             self.first = new_node
             self.last = new_node
         else:
             assert self.last is LinkedListNode
             self.last.next = new_node
             self.last = new_node
-        self.legth += 1
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        assert self.first is LinkedListNode
+        node = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            node.next = None
+        self.length -= 1
+
+        return node
