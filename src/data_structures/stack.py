2b5496fb1bece2a15d8a466a1cd6cc5fe0b91460
Flavio Barboza <flavio.barboza@gmail.com>
2025-06-29 21:54:33 -0600

    stack pop implementation

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -2,6 +2,8 @@ from data_structures.linked_list_node import LinkedListNode
 
 class Stack:
     def __init__(self, value) -> None:
+        self.top: LinkedListNode | None = None
+        self.height = 0
         if value:
             self.push(value)
 
@@ -15,3 +17,15 @@ class Stack:
             new_node.next = current_top
             self.top = new_node
         self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+
+        node = self.top
+        assert node is LinkedListNode
+        self.top = node.next
+        node.next = None
+        self.height -= 1
+
+        return node
