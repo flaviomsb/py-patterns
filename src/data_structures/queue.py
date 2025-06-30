c8c19d96c1006a447be9767a1ea40bb25956fdab
Flavio Barboza <flavio.barboza@gmail.com>
2025-06-29 22:13:27 -0600

    queue enqueue method and constructor revamp

--- src/data_structures/queue.py
+++ src/data_structures/queue.py
@@ -1,10 +1,19 @@
 from data_structures.linked_list_node import LinkedListNode
 
 class Queue:
-    def __init__(self, value):
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.legth = 0
+    
+    def enqueue(self, value):
         new_node = LinkedListNode(value)
-        self.first = new_node
-        self.last = new_node
-        self.legth = 1
-    
-    
+
+        if self.legth == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            assert self.last is LinkedListNode
+            self.last.next = new_node
+            self.last = new_node
+        self.legth += 1
